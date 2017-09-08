3cfd803be26eb8811574b01fa8f8a0710438ec7e
Jack Anderson <jack.anderson@incontact.com>
2017-09-08 08:26:01 -0600

    fixing oauthtoken

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/reset', function(req, res) {
 function sendSQSMessage(result) {
   var params = {
       MessageBody: result,
-      QueueUrl: 'https://sqs.us-west-2.amazonaws.com/386908507125/Showcase1.fifo', //TODO: dynamic URL
+      QueueUrl: 'https://sqs.us-west-2.amazonaws.com/386908507125/Showcase3.fifo', //TODO: dynamic URL
       DelaySeconds: 0,
       MessageAttributes: {
           'Result': {
@@ -47,10 +47,9 @@ function sendSQSMessage(result) {
 }
 
 function resetPipeline() {
-  var oauthToken = "c974ef9809f909e8ea8b451d5587782edd892513";
   var gitHubUser = "derickwalburger";
   var gitHubRepo = "tech-showcase-1";
-  var fileToUpdate = "Showcase1/file.txt";  // Path to file in repo without leading slash
+  var fileToUpdate = "Showcase3/file.txt";  // Path to file in repo without leading slash
   
   var commitMessage = "yolo reset";
   var fileContent = base64.encode((Math.random()*1000000).toString());   // Base 64 encoded content
